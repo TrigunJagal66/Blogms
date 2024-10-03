368885ecb9e0b5beec5c686f3fb2474134f10fc9
TrigunJagal66 <trigunjagal11@gmail.com>
2024-10-03 14:09:10 +0530

    shift mongodb to online server

--- index.js
+++ index.js
@@ -2,39 +2,48 @@ const express = require('express');
 const path = require('path');
 const mongoose = require("mongoose");
 const userRoute = require('./routes/user');
-const blogRoute=require("./routes/blog");
-const cookieParser = require('cookie-parser'); // Fixed typo
-const Blog=require('./models/blog');
+const blogRoute = require("./routes/blog");
+const cookieParser = require('cookie-parser'); 
+const Blog = require('./models/blog');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
+require('dotenv').config();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-mongoose.connect('mongodb://localhost:27017/blogify')
+// MongoDB Atlas URL
+const MONGODB_URL = process.env.MONGODB_URL;
+
+// Connecting to MongoDB Atlas (no deprecated options)
+mongoose.connect(MONGODB_URL)
     .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error('MongoDB connection error:', err)); // Added error handling
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(checkForAuthenticationCookie("token")); // Ensure validateToken is imported
+app.use(checkForAuthenticationCookie("token"));
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.resolve('./public')));
 
-app.get('/', async(req, res) => {
-    const allBlogs=await Blog.find({});
-    res.render('home', {
-        user: req.user,
-        blogs:allBlogs, // This will be `undefined` if not authenticated
-    });
+app.get('/', async (req, res) => {
+    try {
+        const allBlogs = await Blog.find({});
+        res.render('home', {
+            user: req.user,
+            blogs: allBlogs,
+        });
+    } catch (error) {
+        res.status(500).send("Error fetching blogs");
+    }
 });
 
 app.use("/user", userRoute);
-app.use("/blog",blogRoute);
+app.use("/blog", blogRoute);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
@@ -43,3 +52,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+
+
